1439884eefa03527f36315625cc4a8cd296a0736
dwcalade <dwcalade@yahoo.ca>
2022-04-10 23:20:10 -0700

    commit

---
+++ test/arrayTest.js
@@ -0,0 +1,24 @@
+describe('Array', function() {
+    // Further code for tests goes here
+  });
+
+
+
+  describe('Array', function() {
+    it('should start empty', function() {
+      // Test implementation goes here
+    });
+  
+    // We can have more its here
+  });
+
+
+  var assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
---
+++ test/classNameTest.js
@@ -0,0 +1,20 @@
+
+
+ //using mocha in node
+
+
+
+
+  // classNameTest.js
+
+  var chai = require('chai');
+  var assert = chai.assert;
+  
+  var className = require('../js/className.js');
+  var addClass = className.addClass;
+  
+  // The rest of the file remains the same
+  
+  describe('addClass', function() {
+  
+  });
