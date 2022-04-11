fc37d8f5a2bb329dbd086c754acc4a854ab319c6
dwcalade <dwcalade@yahoo.ca>
2022-04-11 20:12:00 +0000

    Refactor middle

---
+++ middle.js
@@ -0,0 +1,18 @@
+
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = require('./assertArraysEqual');
+
+const middle = function(arr) {
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 === 0) {
+    let A = arr.length / 2 - 1;
+    let B = arr.length / 2;
+    return [arr[A], arr[B]];
+  } else {
+    return [arr[Math.floor(arr.length / 2)]];
+  }
+};
+
+module.exports = middle;
